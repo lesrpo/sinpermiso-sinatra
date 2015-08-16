da8ff35a4d6adde57d64779982398b2f8c9b3421
lesrpo <leslierestrepo@gmail.com>
2015-08-16 09:50:42 +0400

    Initial commit

---
+++ solution.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'rubygems'
+require 'net/http'
+
+get '/' do
+	#headers['HTTP_PERMISO']== "soy-un-token-secreto"?"Si lo logramos":"Sin permiso"
+	# request.headers['PERMISO']
+	request.env['HTTP_PERMISO']== "soy-un-token-secreto"?"Si lo logramos ":"Sin permiso "
+end
