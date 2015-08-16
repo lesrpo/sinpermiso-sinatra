ca4dc044cc9025dfea6557187c5ae8d892f39a7d
lesrpo <leslierestrepo@gmail.com>
2015-08-16 09:55:09 +0400

    Add ! symbol to the answer

--- solution.rb
+++ solution.rb
@@ -5,5 +5,5 @@ require 'net/http'
 get '/' do
 	#headers['HTTP_PERMISO']== "soy-un-token-secreto"?"Si lo logramos":"Sin permiso"
 	# request.headers['PERMISO']
-	request.env['HTTP_PERMISO']== "soy-un-token-secreto"?"Si lo logramos ":"Sin permiso "
+	request.env['HTTP_PERMISO']== "soy-un-token-secreto"?"Si lo logramos!":"Sin permiso"
 end
